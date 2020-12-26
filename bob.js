6af96e09a5acef12bd18353468618a912ecb4303
AviGoel <74290197+AviGoel@users.noreply.github.com>
2020-12-26 21:45:18 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,26 @@
+class Bob{
+    constructor(x,y,r){
+        var options = {
+             isStatic:true,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+    
+    this.x=x;
+    this.y=y;
+    this.r=r;
+
+    this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+    World.add(world,this.body)
+    }
+    display(){
+     push()
+     translate(this.body.position.x,this.body.position.y)
+     rotate(this.body.angle);
+     strokeWeight(2);
+     fill(0,0,0);
+     ellipse(0,0,this.r,this.r);
+     pop()
+    }
+}
---
+++ rope.js
@@ -0,0 +1,24 @@
+class Rope {
+    constructor(bodyA,bodyB,offsetX,offsetY){
+        this.offsetX=offsetX;
+        this.offsetY=offsetY;
+        
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            length:15,
+            stiffness:1,
+            pointB:{x:this.offsetX,y:this.offsetY}
+        }
+
+        this.rope=Constraint.create(options);
+
+        World.add(world,this.rope);
+    }
+    
+    display(){
+     var posA=this.rope.bodyA.position;
+     var posB=this.rope.bodyB.position; 
+     line(posA.x,posA.y,posB.x+this.offsetX,posB.y+this.offsetY);
+    }
+} 
